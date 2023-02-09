203c700e78034b223796e45476159c013379b3bc
Simone Marcantel <simonemarcantel04@gmail.com>
2023-02-08 21:41:32 -0600

    changes

---
+++ 02_ArtHubSeedData.sql
@@ -0,0 +1,26 @@
+﻿USE [ArtHub];
+GO
+
+set identity_insert [Listing] on
+insert into Listing (Id, Description, UserId)
+values (1, 'This is the first piece I made my senior year of highschool.', 1)
+
+set identity_insert [Listing] off
+
+set identity_insert [ListingMedium] on
+insert into ListingMedium (Id, ListingId, MediumId)
+values (1, 1, 1)
+
+set identity_insert [ListingMedium] off
+
+set identity_insert [ListingTag] on
+insert into ListingTag (Id, ListingId, TagId)
+values (1, 1, 1)
+
+set identity_insert [ListingTag] off
+
+set identity_insert [User] on
+insert into [User] (Id, FirebaseUserId, FirstName, LastName, UserName, Email)
+values (1, '7LBFCqJFSpNgzY3zZh4lLDugiSX2', 'Simone', 'Marcantel', 'sm.art', 'simone@gmail.com')
+
+set identity_insert [User] off
---
+++ SQLQuery1.sql
@@ -0,0 +1,77 @@
+﻿USE [master]
+
+IF db_id('ArtHub') IS NULL
+	CREATE DATABASE [ArtHub]
+GO
+
+USE [ArtHub]
+GO
+
+DROP TABLE IF EXISTS [Listing];
+DROP TABLE IF EXISTS [User];
+DROP TABLE IF EXISTS [Tag];
+DROP TABLE IF EXISTS [Medium];
+DROP TABLE IF EXISTS [ListingTag];
+DROP TABLE IF EXISTS [ListingMedium];
+GO
+
+
+CREATE TABLE [Listing] (
+  [Id] int IDENTITY(1, 1),
+  [Description] nvarchar(255),
+  [UserId] int IDENTITY(1, 1),
+  PRIMARY KEY ([Id], [UserId])
+)
+GO
+
+CREATE TABLE [User] (
+  [Id] int IDENTITY(1, 1),
+  [FirebaseUserId] int IDENTITY(1, 1),
+  [FirstName] nvarchar(255) NOT NULL,
+  [LastName] nvarchar(255) NOT NULL,
+  PRIMARY KEY ([Id], [FirebaseUserId])
+)
+GO
+
+CREATE TABLE [Tag] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [Name] nvarchar(255) NOT NULL
+)
+GO
+
+CREATE TABLE [Medium] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [Name] nvarchar(255) NOT NULL
+)
+GO
+
+CREATE TABLE [ListingTag] (
+  [Id] int IDENTITY(1, 1),
+  [ListingId] int IDENTITY(1, 1),
+  [TagId] int IDENTITY(1, 1),
+  PRIMARY KEY ([Id], [ListingId], [TagId])
+)
+GO
+
+CREATE TABLE [ListingMedium] (
+  [Id] int IDENTITY(1, 1),
+  [ListingId] int IDENTITY(1, 1),
+  [MediumId] int IDENTITY(1, 1),
+  PRIMARY KEY ([Id], [ListingId], [MediumId])
+)
+GO
+
+ALTER TABLE [ListingTag] ADD FOREIGN KEY ([ListingId]) REFERENCES [Listing] ([Id]) ON DELETE CASCADE
+GO
+
+ALTER TABLE [ListingMedium] ADD FOREIGN KEY ([ListingId]) REFERENCES [Listing] ([Id]) ON DELETE CASCADE
+GO
+
+ALTER TABLE [Tag] ADD FOREIGN KEY ([Id]) REFERENCES [ListingTag] ([TagId])
+GO
+
+ALTER TABLE [Listing] ADD FOREIGN KEY ([UserId]) REFERENCES [User] ([Id])
+GO
+
+ALTER TABLE [Medium] ADD FOREIGN KEY ([Id]) REFERENCES [ListingMedium] ([MediumId])
+GO
